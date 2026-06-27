fc553c1d7434d7318c35a0740352d80c7d94dd3d
Gaurav836832 <146833253+Gaurav836832@users.noreply.github.com>
2026-06-27 12:14:24 +0530

    Remove comments

--- switch Statement.js
+++ switch Statement.js
@@ -1,29 +1,24 @@
-// // write a function where user gave a coffe name and according to coffetype function return rate of coffe.
+function coffeMachine(coffetype) {
+    let coffename = coffetype.toLowerCase()
+    let answer;
+    switch (coffename) {
 
+        case "latte":
+            answer = "$4"
+            break;
 
-// function coffeMachine(coffetype) {
-//     let coffename = coffetype.toLowerCase()
-//     let answer;
-//     switch (coffename) {
+        case "capachino":
+            answer = "$5"
+            break;
 
-//         case "latte":
-//             answer = "$4"
-//             break;
+        case "starbucks":
+            answer = "$9"
+            break;
 
-//         case "capachino":
-//             answer = "$5"
-//             break;
+        default: answer = "Invalid coffe Type"
+    }
+    return answer
 
-//         case "starbucks":
-//             answer = "$9"
-//             break;
+}
 
-//         default: answer = "Invalid coffe Type"
-//     }
-//     return answer
-
-// }
-
-// console.log(coffeMachine("Capachino"))
-
-
+console.log(coffeMachine("Capachino"))
