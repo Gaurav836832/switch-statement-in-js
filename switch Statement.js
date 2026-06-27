001145bb9ee05180e841348509cfc264a805f3fb
Gaurav836832 <146833253+Gaurav836832@users.noreply.github.com>
2026-06-27 12:12:49 +0530

    Add files via upload

---
+++ switch Statement.js
@@ -0,0 +1,29 @@
+// // write a function where user gave a coffe name and according to coffetype function return rate of coffe.
+
+
+// function coffeMachine(coffetype) {
+//     let coffename = coffetype.toLowerCase()
+//     let answer;
+//     switch (coffename) {
+
+//         case "latte":
+//             answer = "$4"
+//             break;
+
+//         case "capachino":
+//             answer = "$5"
+//             break;
+
+//         case "starbucks":
+//             answer = "$9"
+//             break;
+
+//         default: answer = "Invalid coffe Type"
+//     }
+//     return answer
+
+// }
+
+// console.log(coffeMachine("Capachino"))
+
+
